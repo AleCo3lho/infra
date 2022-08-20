908587b4ee4b4e0d99b742522ce826b2fd696407
AleCo3lho <alexandre_ramos007@hotmail.com>
2022-08-20 18:06:17 -0400

    primeiro update

---
+++ aks.tf
@@ -0,0 +1,36 @@
+resource "azurerm_resource_group" "rg" {
+  name     = "rg-coelhostuff-dev-004"
+  location = "East US"
+}
+
+resource "azurerm_kubernetes_cluster" "aks" {
+  name                = "aks-coelhostuff-dev-004"
+  location            = azurerm_resource_group.rg.location
+  resource_group_name = azurerm_resource_group.rg.name
+  dns_prefix          = "akscoelhostuffdev004"
+  kubernetes_version  = "1.22.6"
+
+
+  default_node_pool {
+    name       = "default"
+    node_count = 1
+    vm_size    = "Standard_D4s_v3"
+    max_pods   = 240
+    os_sku     = "Ubuntu"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+
+  tags = {
+    Environment = "Development"
+  }
+}
+
+resource "azurerm_role_assignment" "ara" {
+  principal_id                     = azurerm_kubernetes_cluster.aks.kubelet_identity[0].object_id
+  role_definition_name             = "AcrPull"
+  scope                            = azurerm_container_registry.acr.id
+  skip_service_principal_aad_check = true
+}
---
+++ np-efk.tf
@@ -0,0 +1,20 @@
+/* resource "azurerm_kubernetes_cluster_node_pool" "efk" {
+  name                  = "efk"
+  kubernetes_cluster_id = azurerm_kubernetes_cluster.aks.id
+  vm_size               = "Standard_D4s_v3"
+  max_pods              = 240
+  os_sku                = "Ubuntu"
+  node_count            = 1
+
+  tags = {
+    env  = "dev"
+    role = "efk"
+    tier = "application"
+  }
+  node_labels = {
+    app1 = "elastic"
+    app2 = "kibana"
+    role = "efk"
+    env  = "dev"
+  }
+} */
