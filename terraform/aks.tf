d6e44ffc5e89628e247323ec4e03d5888be19fb6
AleCo3lho <alexandre_ramos007@hotmail.com>
2022-09-13 10:02:58 -0400

    update default np

--- terraform/aks.tf
+++ terraform/aks.tf
@@ -13,8 +13,8 @@ resource "azurerm_kubernetes_cluster" "aks" {
 
   default_node_pool {
     name       = "default"
-    node_count = 1
-    vm_size    = "Standard_B2ms"
+    node_count = 2
+    vm_size    = "Standard_B2s"
     max_pods   = 240
     os_sku     = "Ubuntu"
   }
