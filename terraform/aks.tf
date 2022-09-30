696472a6e41d96c0ae0903e8a78ba45076f8df22
AleCo3lho <alexandre_ramos007@hotmail.com>
2022-09-30 15:03:35 -0400

    alterando versão kubernetes

--- terraform/aks.tf
+++ terraform/aks.tf
@@ -8,7 +8,7 @@ resource "azurerm_kubernetes_cluster" "aks" {
   location            = azurerm_resource_group.rg.location
   resource_group_name = azurerm_resource_group.rg.name
   dns_prefix          = "akscoelhostuffdev004"
-  kubernetes_version  = "1.22.6"
+  kubernetes_version  = "1.24.3"
 
 
   default_node_pool {
